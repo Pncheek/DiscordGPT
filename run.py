611f3e0c57c3dbfbb324b37594a2d88acbd4b52e
Pncheeck <159966542+Pncheek@users.noreply.github.com>
2024-03-31 01:45:20 +0300

    adding image generetion function
    added "!image" function, wich generating images. Also just ofund out that my dall-e 3 rate is 0/minute images lol (in docs i did read that my rate in 1/minute so i am very confused about it)

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
+
 import discord
 from discord.ext import commands
+import openai
 from openai import OpenAI
 
 #Установка токена
 DISCORD_TOKEN = "MTIyMjg3NjUxMDAzNDMyOTcyMQ.G7yJ-v.OP4YMpyaWfsdWX3nikyzjBlR2rkJI84TYjrp0w"
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = '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'
 
 #Подключение openAI
 client = OpenAI(api_key=OPENAI_API_KEY)
@@ -26,12 +28,29 @@ async def chat(ctx, *, prompt):
             {"role": "system", "content": "Тебе необходимо поддерживать диалог. Ты должен запоминать предедущие отправленные тебе сообщения и брать из них контекст для вопроса и/или ответа"},
             {"role": "assistant", "content": "Тебе необходимо следовать контексту предыдущих сообщений и не повторять вопросы"},
             {"role": "user", "content": prompt}],
-        model="gpt-4",
+        model="gpt-3.5-turbo-0125",
         temperature=1,
         max_tokens=2000
         )
     #Отсылаем ответ в качестве reply
     await ctx.reply(response.choices[0].message.content)
+
+
+@bot.command()
+async def image(ctx, *, prompt):
+    response = client.images.generate(
+        model="dall-e-2", #лимит 5 картинок в минуту
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        
+    )
+    await ctx.reply(response.data[0].url)
+'''это не работает с dalle-3 (пишет, что слишком часто генерю картинки. 
+У далли 3 частота не может быть выше 1 картинки в минуту на бесплатной версии. 
+А я генерил 0, вообще, потому что программа не работала, 
+а на вход в n подаётся количество картинок для генераци, она там одна и короче хз, не работает)'''
+    
     
 
 bot.run(DISCORD_TOKEN)
